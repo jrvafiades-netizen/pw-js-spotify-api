8d4a9aab375b3c8aab1a5dbc7bcdbb7e1c31a7a7
James Vafiades <jrvafiades@gmail.com>
2025-11-26 17:57:07 -0500

    test

--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
-import dotenv from 'dotenv';
-
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
- */
+ */ 
 // import dotenv from 'dotenv';
-// import path from 'path';
+// import path from 'path'; 
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
